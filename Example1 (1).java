0a0fb3542ae8abceaa378c79a1a943fef575ccd2
Bljitji <87616346+Bljitji@users.noreply.github.com>
2021-07-18 14:21:32 -0400

    Add files via upload

---
+++ Example1 (1).java
@@ -0,0 +1,18 @@
+package weekend_classes.july.arrays;
+/**
+ * 
+ * Write a Java program to display the values of a given array.
+ *
+ */
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		int[] marks = {69, 85, 66, 80, 81 };
+	
+		
+		
+	
+		
+	}
+}
